7dbb8bd12a486286f356fc0c8632ad3fee619c2d
abhishekraushan122 <168331535+abhishekraushan122@users.noreply.github.com>
2026-06-06 23:28:36 +0530

    Add DATABASE_URL to database configuration

--- db.js
+++ db.js
@@ -7,7 +7,9 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
   schema: process.env.DB_SCHEMA,
+  dburl:process.env.DATABASE_URL,
    ssl: {
+     require: true,
     rejectUnauthorized: false,
   },
 });
