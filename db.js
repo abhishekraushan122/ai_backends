97a196f433ae16e015dd1d8dba3da722aa5ed0e5
abhishekraushan122 <168331535+abhishekraushan122@users.noreply.github.com>
2026-06-07 13:46:34 +0530

    Refactor database connection configuration

--- db.js
+++ db.js
@@ -1,15 +1,21 @@
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   schema: process.env.DB_SCHEMA,
+//   dburl:process.env.DATABASE_URL,
+//    ssl: {
+//      require: true,
+//     rejectUnauthorized: false,
+//   },
+// });
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  schema: process.env.DB_SCHEMA,
-  dburl:process.env.DATABASE_URL,
-   ssl: {
-     require: true,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
     rejectUnauthorized: false,
   },
 });
