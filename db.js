0d131aaafb250a287e896206363496b58a7411c0
abhishekraushan122 <168331535+abhishekraushan122@users.noreply.github.com>
2026-06-07 13:58:08 +0530

    Change DATABASE_URL to POSTGRES_URL in db.js

--- db.js
+++ db.js
@@ -14,7 +14,7 @@ const { Pool } = require("pg");
 //   },
 // });
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.POSTGRES_URL,
   ssl: {
     rejectUnauthorized: false,
   },
